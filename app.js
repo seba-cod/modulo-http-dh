6705dcceafd684627ae15bc88d83e71480bad10c
seba-cod <sebastiankoleff@gmail.com>
2020-12-02 21:43:36 -0300

    Primer commit ejercitacion modulo HTTP DH

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.join(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => { 
+    console.log("=====================");
+    console.log("http://localhost:3000");
+    console.log("=====================");
+    }
+);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/index.html/"))
+});
