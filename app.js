979dbdd1155ff45e49fe3c0c0082548f646ff494
seba-cod <sebastiankoleff@gmail.com>
2020-12-23 02:08:10 -0300

    updateado con register

--- app.js
+++ app.js
@@ -16,3 +16,7 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "./views/index.html/"))
 });
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html/"))
+});
