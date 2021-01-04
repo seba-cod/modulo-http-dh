93d747734c9e11c493aad83bfbcc2c84e0ae37e4
seba-cod <sebastiankoleff@gmail.com>
2021-01-04 01:12:53 -0300

    actualizado- formularios (registro y login)

--- app.js
+++ app.js
@@ -14,9 +14,14 @@ app.listen(3000, () => {
 );
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/index.html/"))
+    res.sendFile(path.resolve(__dirname, "./views/index.html/"))
 });
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/register.html/"))
+    res.sendFile(path.resolve(__dirname, "./views/register.html/"))
+});
+
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html/"))
 });
